cdcdd11bc0393e78479ef8233fd0d3c67175e649
chinonsopaul <chinonsobezos@gmail.com>
2023-02-17 11:30:31 -0800

    tasks 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,11 @@
 int main(void)
 {
 	char i;
-	char x;
 
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
-	putchar('\n');
-	for (x = 'A'; x <= 'Z'; x++)
-		putchar(x);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 

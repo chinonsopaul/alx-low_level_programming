9c5ea5207790b51870812321d3dca31503039e7b
chinonsopaul <chinonsobezos@gmail.com>
2023-02-17 18:15:07 -0800

    tasks10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 1; x < 10; x++)
+		{
+			if (i < x && i != x)
+			{
+				putchar(i + '0');
+				putchar(x + '0');
+				if (i + x != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

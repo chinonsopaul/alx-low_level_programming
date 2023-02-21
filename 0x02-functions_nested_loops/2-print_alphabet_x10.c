fe6719a628f8b0b965020dee5b846c1a9de800d4
chinonsopaul <chinonsobezos@gmail.com>
2023-02-21 08:28:23 -0800

    a-z 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) - the function i used
+ *
+ * main - main function
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char i2;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (i2 = 'a'; i2 <= 'z'; i2++)
+			_putchar(i2);
+		_putchar('\n');
+	}
+}

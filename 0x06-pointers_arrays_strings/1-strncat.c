76a40c5f50f1ef8a26e6cf68a702e6844469554e
chinonsopaul <chinonsobezos@gmail.com>
2023-03-01 08:19:31 -0800

    task2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,28 @@
 #include "main.h"
 /**
- * *_strncat - a function that concatenates two strings
- *
- * Return: always 0
+ * *_strncpy - a function that copies a string
+ * @dest: inputs
+ * @src: inputs
+ * @n: inputs
+ * Return: always 0 as success
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }

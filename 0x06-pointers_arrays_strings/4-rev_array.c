c04044602feeeae16d24088c27c5a156621eec33
chinonsopaul <chinonsobezos@gmail.com>
2023-03-01 09:06:29 -0800

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,10 +2,19 @@
 /**
  * reverse_array - a function that reverses the content of
  * an array of integers
- *
+ * @a: inputs
+ * @n: second input
  * Return: always 0
  */
 void reverse_array(int *a, int n)
 {
 	int i;
+	int u;
+
+	for (i = 0; i < n--; i++)
+	{
+		u = a[i];
+		a[i] = a[n];
+		a[n] = u;
+	}
 }

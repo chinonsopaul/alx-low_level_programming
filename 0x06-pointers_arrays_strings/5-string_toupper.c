6529d32979cf7fdd280b56038b4bc6170cd9f56a
chinonsopaul <chinonsobezos@gmail.com>
2023-03-02 02:15:49 -0800

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,6 +2,7 @@
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
+ *
  * Return: n
  */
 char *string_toupper(char *n)
@@ -12,10 +13,8 @@ char *string_toupper(char *n)
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-		{
 			n[i] = n[i] - 32;
-			i++;
-		}
+		i++;
 	}
 	return (n);
 }

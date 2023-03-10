5f2f447d2b306a7fcb700158d6dd866a7cb88d1f
chinonsopaul <chinonsobezos@gmail.com>
2023-03-10 14:08:26 -0800

    yeah

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
